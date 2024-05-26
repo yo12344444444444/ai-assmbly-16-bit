996bfd0386ba9565dee89a54ff5d5b0b144f5738
yo12344444444444 <100028795+yo12344444444444@users.noreply.github.com>
2024-05-26 22:13:46 +0300

    Update hi.asm

--- hi.asm
+++ hi.asm
@@ -14,11 +14,15 @@ DATASEG
     sizeOffKiller db 120
     hiddenSize dw 9
     outputSiz dw 1
-    ErrorMsg db "error"
-    Filename db "file.txt",0
-    filehandle dw ? 
     Clock equ es:6Ch
-    caunter dw , 0
+    caunter dw  0
+    bise dw  355
+    filename db 'bagrond.bmp',0
+    filehandle dw ?
+    Header db 54 dup (0)
+    Palette db 256*4 dup (0)
+    ScrLine db 320 dup (0)
+    ErrorMsg db 'Error', 13, 10 ,'$'
 SEGMENT hiddenSeg
 input db 0,0,126
 wights db 2,0,2
@@ -31,6 +35,100 @@ SEGMENT bgSeg
     dB 320*200 dup(32)
 ENDS bgSeg
 CODESEG
+proc OpenFile
+    ; Open file
+    mov ah, 3Dh
+    xor al, al
+    mov dx, offset filename
+    int 21h
+    jc openerror
+    mov [filehandle], ax
+    ret
+    openerror:
+        mov dx, offset ErrorMsg
+        mov ah, 9h
+        int 21h
+    ret
+endp OpenFile
+proc ReadHeader
+    ; Read BMP file header, 54 bytes
+    mov ah,3fh
+    mov bx, [filehandle]
+    mov cx,54
+    mov dx,offset Header
+    int 21h
+    ret
+endp ReadHeader 
+proc ReadPalette
+    ; Read BMP file color palette, 256 colors * 4 bytes (400h)
+    mov ah,3fh
+    mov cx,400h
+    mov dx,offset Palette
+    int 21h
+    ret
+endp ReadPalette
+proc CopyPal
+    ; Copy the colors palette to the video memory
+    ; The number of the first color should be sent to port 3C8h
+    ; The palette is sent to port 3C9h
+    mov si,offset Palette
+    mov cx,256
+    mov dx,3C8h
+    mov al,0
+    ; Copy starting color to port 3C8h
+    out dx,al
+    ; Copy palette itself to port 3C9h
+    inc dx
+    PalLoop:
+        ; Note: Colors in a BMP file are saved as BGR values rather than RGB .
+        mov al,[si+2] ; Get red value .
+        shr al,2 ; Max. is 255, but video palette maximal
+        ; value is 63. Therefore dividing by 4.
+        out dx,al ; Send it .
+        mov al,[si+1] ; Get green value .
+        shr al,2
+        out dx,al ; Send it .
+        mov al,[si] ; Get blue value .
+        shr al,2
+        out dx,al ; Send it .
+        add si,4 ; Point to next color 
+    loop PalLoop
+    ret
+endp CopyPal
+proc CopyBitmap
+    ; BMP graphics are saved upside-down .
+    ; Read the graphic line by line (200 lines in VGA format),
+    ; displaying the lines from bottom to top.
+    mov ax, bgSeg
+    mov es, ax
+    mov cx,200
+    PrintBMPLoop:
+    push cx
+    ; di = cx*320, point to the correct screen line
+    mov di,cx
+    shl cx,6
+    shl di,8
+    add di,cx
+    ; Read one line
+    mov ah,3fh
+    mov cx,320
+    mov dx,offset ScrLine
+    int 21h
+    ; Copy one line into video memory
+    cld ; Clear direction flag, for movsb
+    mov cx,320
+    mov si,offset ScrLine
+    rep movsb ; Copy line to the screen
+    ;rep movsb is same as the following code :
+    ;mov es:di, ds:si
+    ;inc si
+    ;inc di
+    ;dec cx
+    ;loop until cx=0
+    pop cx
+    loop PrintBMPLoop
+    ret
+endp CopyBitmap
 proc bagrond
     push ax
     push di
@@ -280,17 +378,17 @@ proc move
     ja cnc
     call newPIP2
     cnc:
-    mov ax ,0
-    mov al, [byte offset place] 
-    mov cl , 200
-    mul cl
-    mov di , ax
-    mov ax, outputSeg
-    mov es, ax
-    mov si , [es:0]
-    cmp si, 355 
-    jle down
-    ja up
+        mov ax ,0
+        mov al, [byte offset place] 
+        mov cl , 200
+        mul cl
+        mov di , ax
+        mov ax, outputSeg
+        mov es, ax
+        mov si , [es:0]
+        cmp si, [offset bise] 
+        jle down
+        ja up
     down:     
         cmp [byte offset place], 220
         je action
@@ -356,6 +454,11 @@ start:
     mov ds, ax
     mov ax, 13h
     int 10h
+    call OpenFile
+    call ReadHeader
+    call ReadPalette
+    call CopyPal
+    call CopyBitmap
     mov ax, 0A000h 
     mov es, ax
 inf:
